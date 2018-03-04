494e657426ac049e0e4091df212c2c9e76766bd2
Yavuz Selim Yesilyurt <yesilyurt.selim@metu.edu.tr>
2018-03-04 20:57:51 +0300

    Bug Fixes

--- ptl-C++/Node.hpp
+++ ptl-C++/Node.hpp
@@ -6,66 +6,63 @@
 namespace ptl {
 
     template<class T>
+    class Node;
+
+    template<class T>
+    std::ostream &operator<<(std::ostream &out, const Node<T> &n);
+
+    template<class T>
     class Node {
 
-    private:
+        private:
+            Node<T> *next;
+            T data;
 
-        Node<T> *next;
-        T data;
+        public:
 
-    public:
+            Node();
+            Node(const T &d);
 
-        Node();
-        Node(const T& d);
+            Node<T> *getNext() const;
+            T getData() const;
+            void setNext(Node<T> *newNext);
 
-        Node<T>* getNext() const;
-        T getData() const;
-        void setNext(Node<T> *newNext);
-
-        friend std::ostream &operator<< <> (std::ostream &out, const Node<T>& n);
+            friend std::ostream &operator<< <> (std::ostream &out, const Node<T> &n);
 
     };
 
     template<class T>
-    std::ostream &operator<<(std::ostream &out, const Node<T>& n);
-
-    template <class T>
-    Node<T>::Node()
-    {
+    Node<T>::Node() {
         data = T();
         next = NULL;
     }
 
-    template <class T>
-    Node<T>::Node(const T& d )
-    {
+    template<class T>
+    Node<T>::Node(const T &d) {
         data = d;
         next = NULL;
     }
 
-    template <class T>
-    Node<T>* Node<T>::getNext() const
-    {
+    template<class T>
+    Node<T> *Node<T>::getNext() const {
         return next;
     }
 
-    template <class T>
-    T Node<T>::getData() const
-    {
+    template<class T>
+    T Node<T>::getData() const {
         return data;
     }
 
-    template <class T>
-    void Node<T>::setNext(Node<T> *newNext)
-    {
-        next=newNext;
+    template<class T>
+    void Node<T>::setNext(Node<T> *newNext) {
+        next = newNext;
     }
 
     template<class T>
-    std::ostream &operator<<(std::ostream &out, const Node<T>& n){
+    std::ostream &operator<<(std::ostream &out, const Node<T> &n) {
         out << n.data;
         return out;
-    }
+    };
 }
 
 #endif /* _PTL_NODE_HPP_ */
