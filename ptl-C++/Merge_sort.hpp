28a0453624baec6301d4362d615617f9c24eccc3
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2018-10-11 00:47:39 +0300

    Update Comments

--- ptl-C++/Merge_sort.hpp
+++ ptl-C++/Merge_sort.hpp
@@ -19,9 +19,11 @@ namespace ptl {
     template <typename T>
     void merge_sort(T *arr, size_t start_index, size_t end_index) {
 
-        if (start_index < end_index) {
+        if (start_index < end_index) { 
+            // divide 
             merge_sort(arr, start_index, (end_index + start_index) / 2); // middle = (end + start)/2
             merge_sort(arr, (end_index + start_index) / 2 + 1, end_index);
+            // and conquer
             merge_for_sort(arr, start_index, (end_index + start_index) / 2, end_index);
         }
     }
@@ -32,15 +34,18 @@ namespace ptl {
         size_t len1 = middle - first + 1, len2 = last - middle, arr_index = first, arr1_index = 0, arr2_index = 0;
         size_t arr1[len1], arr2[len2];
 
-        for (int l = 0; l < len1; ++l) { // create arr1 with first half
+        for (int l = 0; l < len1; ++l) { 
+            // fill arr1 with first half
             arr1[l] = arr[first + l];
         }
-        for (int l = 0; l < len2; ++l) { // create arr2 with second half
+        for (int l = 0; l < len2; ++l) { 
+            // fill arr2 with second half
             arr2[l] = arr[middle + 1 + l];
         }
 
-        while ((arr1_index < len1) && (arr2_index < len2)) // merge arr1 and arr2 to arr in a sorted manner
+        while ((arr1_index < len1) && (arr2_index < len2)) 
         {
+            // merge arr1 and arr2 to arr in a sorted manner
             if (arr1[arr1_index] <= arr2[arr2_index]) {
                 arr[arr_index] = arr1[arr1_index];
                 arr_index++;
@@ -53,12 +58,14 @@ namespace ptl {
             }
         }
 
-        if (arr1_index < len1) {                            // if arr1 has some remaining numbers push them to arr
+        if (arr1_index < len1) {                            
+            // if arr1 has some remaining numbers push them to arr
             for (; arr1_index < len1; ++arr1_index, ++arr_index) {
                 arr[arr_index] = arr1[arr1_index];
             }
         }
-        else {                                              // if arr2 has some remaining numbers push them to arr
+        else {                                              
+            // if arr2 has some remaining numbers push them to arr
             for (; arr2_index < len2; ++arr2_index, ++arr_index) {
                 arr[arr_index] = arr2[arr2_index];
             }
