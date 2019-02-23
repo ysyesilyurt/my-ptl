59586f88b57783d9e58e17d58e194a451a142527
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-02-23 19:17:46 +0300

    Added include.hpp for usage convenience

---
+++ C++/ptl/include.hpp
@@ -0,0 +1,25 @@
+#ifndef _PTL_INCLUDE_HPP_
+#define _PTL_INCLUDE_HPP_
+
+/* Include this header only for using whole library
+ * without including individual headers manually.
+ * */
+
+#include "AvlTree.hpp"
+#include "Binary_search.hpp"
+#include "Bubble_sort.hpp"
+#include "Counting_sort.hpp"
+#include "HashTable.hpp"
+#include "Heap_sort.hpp"
+#include "Insertion_sort.hpp"
+#include "LinkedList.hpp"
+#include "LinkedList.hpp"
+#include "Merge_sort.hpp"
+#include "Node.hpp"
+#include "Quick_sort.hpp"
+#include "Radix_sort.hpp"
+#include "Selection_sort.hpp"
+#include "Shell_sort.hpp"
+#include "Stack.hpp"
+
+#endif /* _PTL_INCLUDE_HPP_ */
